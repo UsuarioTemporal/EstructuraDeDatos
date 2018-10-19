bb264c137cc496fb6c7aea251575ba44b4154ed5
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-10-19 17:52:17 -0500

    Se comenzó a analizar

--- src/ProgramacionGenericaParaElPryectoFinal/Template.cpp
+++ src/ProgramacionGenericaParaElPryectoFinal/Template.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
 using namespace std;
 template <class T> class Numeros{
+	T dato1,dato2;
 	public :
-		T dato;
+		Numeros(T dato1,T dato2){
+			this->dato1=dato1;
+			this->dato2=dato2;
+		}
+		T mayor(){
+			return dato1>dato2 ? dato1: dato2;
+		}
 };
 int main(){
-	Numeros<int> hola=Numeros();
-	
+	Numeros<int> hola(8,15);
+	cout<<hola.mayor();
 }
