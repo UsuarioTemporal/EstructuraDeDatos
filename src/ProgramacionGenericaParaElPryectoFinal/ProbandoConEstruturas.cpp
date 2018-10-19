504e7d8588e393df2f4d548758656e6032136389
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-10-19 18:11:46 -0500

    Realizando pruebas para el proyecto final

--- src/ProgramacionGenericaParaElPryectoFinal/ProbandoConEstruturas.cpp
+++ src/ProgramacionGenericaParaElPryectoFinal/ProbandoConEstruturas.cpp
@@ -1,5 +1,40 @@
 #include<iostream>
 using namespace std;
+template <class T>class Nodo{
+	public :
+		T dato;
+		Nodo *siguiente=NULL;
+};
+template <class T>class Lista{
+	public :
+		Nodo<T> *inicio,*final;
+		Lista(){
+			inicio=final=NULL;
+		}
+		void insertToTheStart(T dato){
+			Nodo<T> *nuevo1;
+			nuevo1->dato=dato;
+			if(inicio==NULL){
+				inicio=final=nuevo1;
+			}else{
+				nuevo1->siguiente=inicio;
+				inicio=nuevo1;
+			}
+		}
+		void show(){
+			if(!inicio==NULL){
+				Nodo<T> *recorrer;
+				recorrer = inicio;
+				while(recorrer!=NULL){
+					cout<<"["<<recorrer->dato<<"]-->";
+					recorrer=recorrer->siguiente;
+				}
+				cout<<" || "<<endl;
+			}else{
+				cout<<"\nVacio"<<endl;
+			}
+		}
+};
 int main(){
 	
 }
