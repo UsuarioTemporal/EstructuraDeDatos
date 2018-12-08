2ffa2ed9f06fabfb6461bc8a2e59f1c1677ede9b
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-12-07 23:48:07 -0500

    Recordando con crear un grafo

--- src/EjerciciosGrafos/Grafo.h
+++ src/EjerciciosGrafos/Grafo.h
@@ -3,6 +3,12 @@
 #include<isotream>
 using namespace std;
 struct Nodo{
-	
+	char etiqueta;
+	Nodo *siguiente=NULL;
+	Arista *aristaAdyacente;
+};
+struct Arista{
+	Nodo *destino=NULL;
+	Arista *siguiente=NULL;
 };
 #endif GREAFO_H
